ec623cfab919f5cb01692b3806aa99ef56e22b3c
mowenming <mowenming@outlook.com>
2017-07-16 21:49:50 +0800

    添加注释

--- DevCaptureTool/Win32.cs
+++ DevCaptureTool/Win32.cs
@@ -8,8 +8,23 @@ namespace DevCaptureTool
 {
     public class Win32
     {
+        /// <summary>
+        /// 注册全局热键.定义一个系统范围的热键。
+        /// 函数原型：BOOL RegisterHotKey（HWND hWnd，int id，UINT fsModifiers，UINT vk）；
+        /// </summary>
+        /// <param name="hWnd">接收热键产生WM_HOTKEY消息的窗口句柄。若该参数NULL，传递给调用线程的WM_HOTKEY消息必须在消息循环中进行处理。</param>
+        /// <param name="id">定义热键的标识符。调用线程中的其他热键，不能使用同样的标识符。应用程序必须定义一个0X0000-0xBFFF范围的值。一个共享的动态链接库（DLL）必须定义一个范围为0xC000-0xFFFF的值(GlobalAddAtom函数返回该范围）。为了避免与其他动态链接库定义的热键冲突，一个DLL必须使用GlobalAddAtom函数获得热键的标识符。</param>
+        /// <param name="fsModifiers">定义为了产生WM_HOTKEY消息而必须与由nVirtKey参数定义的键一起按下的键。</param>
+        /// <param name="vk">定义热键的虚拟键码。</param>
+        /// <returns>若函数调用成功，返回一个非0值。若函数调用失败，则返回值为0</returns>
         [DllImport("user32.dll")]//注册全局热键
         public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        /// <summary>
+        /// 卸载全局热键.释放调用线程先前登记的热键
+        /// </summary>
+        /// <param name="hWnd">与被释放的热键相关的窗口句柄。若热键不与窗口相关，则该参数为NULL。</param>
+        /// <param name="id">定义被释放的热键的标识符。</param>
+        /// <returns></returns>
         [DllImport("user32.dll")]//卸载全局热键
         public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
--- IPlugins/Win32.cs
+++ IPlugins/Win32.cs
@@ -8,16 +8,65 @@ namespace IPlugins
 {
     public class Win32
     {
+        /// <summary>
+        /// 设置了一个窗口的区域.只有被包含在这个区域内的地方才会被重绘,而不包含在区域内的其他区域系统将不会显示
+        /// </summary>
+        /// <param name="hWnd">窗口的句柄</param>
+        /// <param name="hRgn">指向的区域.函数起作用后将把窗体变成这个区域的形状.如果这个参数是空值,窗体区域也会被设置成空值,也就是什么也看不到.</param>
+        /// <param name="bRedraw">用于设置 当函数起作用后,窗体是不是该重绘一次. true 则重绘,false 则相反,如果你的窗体是可见的,通常建议设置为 true.</param>
+        /// <returns>如果函数执行成功,就返回非零的数字.如果失败,就返回零.</returns>
         [DllImport("user32.dll")]
         public static extern int SetWindowRgn(IntPtr hWnd, IntPtr hRgn, bool bRedraw);
+        /// <summary>
+        /// 创建的一个带圆角的矩形区域
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="cx"></param>
+        /// <param name="cy"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
         [DllImport("gdi32.dll")]
         public static extern IntPtr CreateRoundRectRgn(int x, int y, int cx, int cy, int width, int height);
+        /// <summary>
+        /// 返回hWnd参数所指定的窗口的设备环境获得的设备环境覆盖了整个窗口（包括非客户区），
+        /// 例如标题栏、菜单、滚动条，以及边框。这使得程序能够在非客户区域实现自定义图形，
+        /// 例如自定义标题或者边框。当不再需要该设备环境时，需要调用ReleaseDC函数释放设备环境。
+        /// 注意，该函数只获得通用设备环境，该设备环境的任何属性改变都不会反映到窗口的私有或者类设备环境中（如果窗口有的话）!
+        /// </summary>
+        /// <param name="hWnd">窗口句柄</param>
+        /// <returns></returns>
         [DllImport("user32.dll")]
         public static extern IntPtr GetWindowDC(IntPtr hWnd);
+        /// <summary>
+        /// 释放设备上下文环境（DC）供其他应用程序使用。
+        /// 函数的效果与设备上下文环境类型有关。它只释放公用的和设备上下文环境，对于类或私有的则无效
+        /// 函数原型：int ReleaseDC(HWND hWnd, HDC hdc)；
+        /// </summary>
+        /// <param name="hWnd">指向要释放的设备上下文环境所在的窗口的句柄</param>
+        /// <param name="hDC">指向要释放的设备上下文环境的句柄</param>
+        /// <returns>返回值说明了设备上下文环境是否释放；如果释放成功，则返回值为1；如果没有释放成功，则返回值为0</returns>
         [DllImport("user32.dll")]
         public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+        /// <summary>
+        /// 从当前线程中的窗口释放鼠标捕获，并恢复通常的鼠标输入处理。
+        /// 捕获鼠标的窗口接收所有的鼠标输入（无论光标的位置在哪里），除非点击鼠标键时，光标热点在另一个线程的窗口中。
+        /// 备注：应用程序在调用函数SetCaPture之后调用此函数。
+        /// </summary>
+        /// <returns></returns>
         [DllImport("user32.dll")]
         public static extern bool ReleaseCapture();
+        /// <summary>
+        /// 将指定的消息发送到一个或多个窗口。此函数为指定的窗口调用窗口程序，直到窗口程序处理完消息再返回。
+        /// 而和函数PostMessage不同，PostMessage是将一个消息寄送到一个线程的消息队列后就立即返回。
+        /// LRESULT SendMessage（HWND hWnd，UINT Msg，WPARAM wParam，LPARAM IParam）
+        /// </summary>
+        /// <param name="hwnd">其窗口程序将接收消息的窗口的句柄。如果此参数为HWND_BROADCAST，则消息将被发送到系统中所有顶层窗口，包括无效或不可见的非自身拥有的窗口、被覆盖的窗口和弹出式窗口，但消息不被发送到子窗口。</param>
+        /// <param name="wMsg">指定被发送的消息。</param>
+        /// <param name="wParam">指定附加的消息特定信息。</param>
+        /// <param name="lParam">指定附加的消息特定信息。</param>
+        /// <returns></returns>
         [DllImport("user32.dll")]
         public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
 
@@ -44,13 +93,21 @@ namespace IPlugins
 
         public const int WM_SYSCOMMAND = 0x0112;
         public const int SC_MOVE = 0xF010;
-
+        /// <summary>
+        /// 坐标x,y
+        /// </summary>
         public struct POINT
         {
             public int X;
             public int Y;
         }
-
+        /// <summary>
+        /// 为了控制窗口的大小，在窗口初始化时，需要用到MINMAXINFO结构体。
+        ///  ptMaxSize：  设置窗口最大化时的宽度、高度
+        ///  ptMaxPosition： 设置窗口最大化时x坐标、y坐标
+        ///  ptMinTrackSize： 设置窗口最小宽度、高度
+        ///  ptMaxTrackSize：设置窗口最大宽度、高度
+        /// </summary>
         public struct MINMAXINFO
         {
             public POINT ptReserved;
